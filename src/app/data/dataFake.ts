e609f8db9e434f0da03520f172dadd35c4f721b1
Mayra Kisch <mayrakisch@gmail.com>
2023-04-11 01:54:06 -0300

    creating an alternative router for images

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,31 @@
 export const dataFake = [
   {
-    'id': 1,
-    'photo': 'oioioi',
-    'title': "desenho acima",
-    'description': 'como fiz o desenho'
+    'id': "1",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/camaleao.jpg',
+    'title': "Camaleão",
+    'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
   },
   {
-    'id': 2,
-    'photo': 'oiodvdvioi',
-    'title': "desenho acimaaaaa",
-    'description': 'como fiz o desenhoooooo'
-  }
+    'id': "2",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/Alfafa.jpg',
+    'title': "Coelho Chernobil",
+    'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
+  },
+  {
+    'id': "3",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/pato.png',
+    'title': "Pato",
+    'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
+  },
+  {
+    'id': "4",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/porquinho-da-india.png',
+    'title': "Porquinho da India",
+    'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
+  },
+
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -9,9 +11,25 @@ export class ContentComponent implements OnInit {
   photoCover:string = './assets/img/camaleao.jpg'
   contentTitle:string ='Camaleão'
   contentDescription:string ='Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua. '
-  constructor() { }
+  private id:string | null="0"
+  constructor(
+    private route: ActivatedRoute,
+  ) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value => this.id = value.get('id'));
+    this.setValuesToComponent(this.id)
+  }
+setValuesToComponent(id:string | null) {
+  const result = dataFake.filter( article => article.id === id)[0]
+
+  this.contentTitle = result.title
+  this.contentDescription = result.description
+  this.photoCover = result.photoCover
+
+
+
+}
 }
