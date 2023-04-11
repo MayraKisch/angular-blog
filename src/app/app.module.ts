7f27b8cc2892296762a7916f522942432e87a833
Mayra Kisch <mayrakisch@gmail.com>
2023-04-11 14:02:25 -0300

    new phots add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,31 +1,73 @@
 export const dataFake = [
   {
     'id': "1",
-    'photo': 'aaaa',
+    'photo': 'camaleao',
     'photoCover': '../assets/img/camaleao.jpg',
     'title': "Camaleão",
     'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
   },
   {
     'id': "2",
-    'photo': 'aaaa',
+    'photo': 'coelho',
     'photoCover': '../assets/img/Alfafa.jpg',
     'title': "Coelho Chernobil",
-    'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro. Foi inspirado em um coelho que pertence à minha cunhada e que sofre de várias doenças e não morre de nenhuma delas. Na imagem, o coelho é retratado com uma expressão de indiferênça. A sua pelagem é áspera e irregular, com algumas áreas sem pelos com algumas feridas. Em torno dele, há uma aura de tristeza e melancolia, que taçvez possa ser só ódio mesmo. O objetivo do desenho foi eternizar o momento em que estes tentáculos apareceram em sua cara, o tornando deveras muito feio, apesar de ser um coelho fofinho.'
   },
   {
     'id': "3",
-    'photo': 'aaaa',
+    'photo': 'pato',
     'photoCover': '../assets/img/pato.png',
     'title': "Pato",
-    'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro. Foi inspirado em artes aquarelas. A tentativa foi apresentar um formato para serem estampas de caneca para uma amiga.'
   },
   {
     'id': "4",
     'photo': 'aaaa',
     'photoCover': '../assets/img/porquinho-da-india.png',
     'title': "Porquinho da India",
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro. Foi inspirado em artes aquarelas. A tentativa foi apresentar um formato para serem estampas de caneca para uma amiga.'
+  },
+  {
+    'id': "5",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/coala.png',
+    'title': "Coala",
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro. Foi inspirado em artes aquarelas. A tentativa foi apresentar um formato para serem estampas de caneca para uma amiga.'
+  },
+  {
+    'id': "6",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/logovet.jpg',
+    'title': "Arara Carindé",
     'description': 'Desenho realizado com mesa digital e MediBang Paint Pro, inspirado na arte de grafite de rua.'
+  },
+  {
+    'id': "7",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/gato.png',
+    'title': "Gato",
+    'description': 'Desenho ilustrativo do meu gato Freddy'
+  },
+  {
+    'id': "8",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/dorival.png',
+    'title': "Dorival - O pato",
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro por um conhecido.'
+  },
+  {
+    'id': "9",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/efelante.png',
+    'title': "Efelante",
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro por um conhecido.'
+  },
+  {
+    'id': "10",
+    'photo': 'aaaa',
+    'photoCover': '../assets/img/xquilo.png',
+    'title': "Xquilo",
+    'description': 'Este desenho foi criado usando uma mesa digital e o software MediBang Paint Pro por um conhecido.'
   },
 
 ]
